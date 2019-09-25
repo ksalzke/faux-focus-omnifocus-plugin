552a46e9a90a50244f22afa9fb4080dddd3714a8
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-09-25 22:51:52 +0100

    General tidy up of comments etc

--- Resources/fauxUnFocus.js
+++ Resources/fauxUnFocus.js
@@ -1,29 +1,20 @@
-/*{
-	"type": "action",
-	"targets": ["omnifocus"],
-	"author": "Kaitlin Salzke",
-	"identifier": "com.KaitlinSalzke.fauxUnFocus",
-	"version": "1.0",
-	"description": "Close out of 'focus' mode.",
-	"label": "Faux UnFocus",
-	"shortLabel": "Faux UnFocus"
-}*/
 var _ = (function() {
-  var action = new PlugIn.Action(function(selection, sender) {
-    unfocusedTag = tagNamed("Unfocused") || new Tag("Unfocused");
+	var action = new PlugIn.Action(function(selection, sender) {
+		unfocusedTag = tagNamed("Unfocused") || new Tag("Unfocused");
 
-    unfocusedTag.tasks.forEach(function(task) {
-      task.project.status = Project.Status.Active;
-    });
-    deleteObject(unfocusedTag);
-  });
+		// mark each project that is tagged with 'Unfocused' as active
+		unfocusedTag.tasks.forEach(function(task) {
+			task.project.status = Project.Status.Active;
+		});
 
-  action.validate = function(selection, sender) {
-    // validation code
-    // selection options: tasks, projects, folders, tags
-    return tagNamed("Unfocused").tasks.length > 0;
-  };
+		// delete the 'Unfocused' tag
+		deleteObject(unfocusedTag);
+	});
 
-  return action;
+	action.validate = function(selection, sender) {
+		return tagNamed("Unfocused").tasks.length > 0;
+	};
+
+	return action;
 })();
 _;
