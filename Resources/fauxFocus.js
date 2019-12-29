f352e9feb19fc3d09f2a1ed07835c25aa869d88a
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2019-12-29 20:38:42 +1030

    Unfocus before focusing if project already focused

--- Resources/fauxFocus.js
+++ Resources/fauxFocus.js
@@ -1,41 +1,49 @@
 var _ = (function() {
-	var action = new PlugIn.Action(function(selection, sender) {
-		config = this.focusConfig;
-		// configure tag used
-		unfocusedTag = config.unfocusedTag();
+  var action = new PlugIn.Action(function(selection, sender) {
+    config = this.focusConfig;
 
-		// get list of selected projects
-		projectsToFocus = selection.projects;
-		// add projects from inside selected folders into list
-		selection.folders.forEach(function(folder) {
-			folder.apply(function(item) {
-				if (item instanceof Project) {
-					projectsToFocus.push(item);
-				}
-			});
-		});
+    // if a project is already unfocused when script is run, run 'Unfocus' script first
+    if (config.unfocusedTag().projects.length > 0) {
+      PlugIn.find("com.KaitlinSalzke.fauxFocus")
+        .action("fauxUnFocus")
+        .perform();
+    }
 
-		// get list of all active projects not in 'focus' list
-		flattenedProjects.forEach(project => {
-			if (
-				project.status === Project.Status.Active &&
-				!projectsToFocus.includes(project)
-			) {
-				// for each active project,
-				// set 'unfocused' tag and put on hold
-				project.task.addTag(unfocusedTag);
-				project.status = Project.Status.OnHold;
-			}
-		});
+    // configure tag used
+    unfocusedTag = config.unfocusedTag();
 
-		// show alert to confirm finished
-		new Alert("Focus", "Script complete.").show();
-	});
+    // get list of selected projects
+    projectsToFocus = selection.projects;
+    // add projects from inside selected folders into list
+    selection.folders.forEach(function(folder) {
+      folder.apply(function(item) {
+        if (item instanceof Project) {
+          projectsToFocus.push(item);
+        }
+      });
+    });
 
-	action.validate = function(selection, sender) {
-		return selection.projects.length > 0 || selection.folders.length > 0;
-	};
+    // get list of all active projects not in 'focus' list
+    flattenedProjects.forEach(project => {
+      if (
+        project.status === Project.Status.Active &&
+        !projectsToFocus.includes(project)
+      ) {
+        // for each active project,
+        // set 'unfocused' tag and put on hold
+        project.task.addTag(unfocusedTag);
+        project.status = Project.Status.OnHold;
+      }
+    });
 
-	return action;
+    // show alert to confirm finished
+    new Alert("Focus", "Script complete.").show();
+  });
+
+  action.validate = function(selection, sender) {
+    return selection.projects.length > 0 || selection.folders.length > 0;
+  };
+
+  return action;
 })();
 _;
--- Resources/fauxUnFocus.js
+++ Resources/fauxUnFocus.js
@@ -1,25 +1,22 @@
 var _ = (function() {
-	var action = new PlugIn.Action(function(selection, sender) {
-		config = this.focusConfig;
-		// configure tag used
-		unfocusedTag = config.unfocusedTag();
+  var action = new PlugIn.Action(function(selection, sender) {
+    config = this.focusConfig;
+    // configure tag used
+    unfocusedTag = config.unfocusedTag();
 
-		// mark each project that is tagged with 'Unfocused' as active
-		unfocusedTag.tasks.forEach(function(task) {
-			task.project.status = Project.Status.Active;
-		});
+    // mark each project that is tagged with 'Unfocused' as active
+    unfocusedTag.tasks.forEach(function(task) {
+      task.project.status = Project.Status.Active;
+    });
 
-		// delete the 'Unfocused' tag
-		deleteObject(unfocusedTag);
+    // delete the 'Unfocused' tag
+    deleteObject(unfocusedTag);
+  });
 
-		// show alert to confirm finished
-		new Alert("Unfocused", "Script complete.").show();
-	});
+  action.validate = function(selection, sender) {
+    return tagNamed("Unfocused").tasks.length > 0;
+  };
 
-	action.validate = function(selection, sender) {
-		return tagNamed("Unfocused").tasks.length > 0;
-	};
-
-	return action;
+  return action;
 })();
 _;
